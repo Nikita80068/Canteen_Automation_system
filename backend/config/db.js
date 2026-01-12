c1e7becbb75aac1d29cce1c1eaea7362a87233af
Nikita Chauhan <nikkiichauhan1001@gmail.com>
2026-01-12 19:09:22 +0530

    modified db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
 export const connectDB=async()=>{
-   await mongoose.connect('mongodb+srv://Tomato_User:abcdefg@cluster0.krwf60w.mongodb.net/food-del').then(()=>console.log("DB Connected"))
+   await mongoose.connect('mongodb+srv://nikkiichauhan1001_db_user:wX9cRVMgi1BgMaMa@cluster0.xex291h.mongodb.net/?appName=Cluster0').then(()=>console.log("DB Connected"))
 }
 
